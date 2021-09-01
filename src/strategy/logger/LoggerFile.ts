014e38898ffcc1fef7dcd949374a34c88f01b13c
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2021-09-01 20:23:50 -0300

    writing in the file

--- src/strategy/logger/LoggerFile.ts
+++ src/strategy/logger/LoggerFile.ts
@@ -1,9 +1,26 @@
+import path from "path";
+import { existsSync, writeFileSync, appendFileSync } from "fs";
+
 import LoggerOutputInterface from "./LoggerOutputInterface";
 
 export default class LogFile implements LoggerOutputInterface {
-  constructor(private filename: string) {}
+
+
+  constructor(private filename: string, private filepath: string) { }
 
   print(message: string): void {
+    const baseDir = path.join(__dirname, this.filepath);
+    const pathfile = `${baseDir}${this.filename}`;
+
+    const fileExists = existsSync(pathfile);
+
+    if(fileExists) {
+      const messageWithLineBreak = `\n\r${message}`;
+      appendFileSync(pathfile, messageWithLineBreak);
+    } else {
+      writeFileSync(pathfile, message);
+    }
+
     console.log(`${message} in file ${this.filename}`);
   }
 }
--- src/strategy/logger/index.ts
+++ src/strategy/logger/index.ts
@@ -7,5 +7,6 @@ const logger = new Logger();
 logger.setLogOutput(new LoggerConsole());
 logger.debug("Debugging");
 
-logger.setLogOutput(new LoggerFile("Test.txt"));
+logger.setLogOutput(new LoggerFile("Test.txt", "/file/"));
 logger.debug("Debugging");
+logger.error("Error on line 43... ");
